58c3c05176a1bfc7748744e7f8a7fd648563f8ad
Cooper Webb <CCWebb14@gmail.com>
2022-02-21 10:37:50 -0800

    removed old functions, scoring schanged to first to five

--- RPS.js
+++ RPS.js
@@ -1,24 +1,11 @@
 
 const buttons = document.querySelectorAll('.button');
 const outputField = document.querySelector('.output-field');
+let wins = 0;
+let losses = 0;
 
-// buttons.forEach(button => button.addEventListener('click', roundOrGame, {
-//     once: true
-// }));
+buttons.forEach(button => button.addEventListener('click', playGame));
 
-buttons.forEach(button => button.addEventListener('click', playRound));
-
-function roundOrGame(){
-    const Response = confirm(
-        "Press \"OK\", if you would like to play a five round game of RPS.\
-\nPress \"Cancel\", if you would only like to play a round.");
-    if (Response) {
-        playGame();
-    }
-    else{
-        playRound();
-    }
-}
 
 function computerPlay(){
     let genNum = Math.floor(Math.random() * 98);
@@ -35,71 +22,37 @@ function computerPlay(){
     return compMove;
 }
 
-// function getID(){
-//     console.log(this.id);
-// }
+function playGame(){
 
-// function getInput(){
-//     outputField.innerText = "Please select an option above";
-//     buttons.forEach(button => button.addEventListener('click', getID));
-// }
-
-function playRound(){
-
-    // let playerMove = (window.prompt("rock, paper or scissors?", "rock")).toLowerCase();
     console.log(this.id);
     let playerMove = this.id;
     let compMove = computerPlay();
     let response = "";
-    let win = 0;
 
-    if ((playerMove != "rock")
-    && (playerMove != "scissors")
-    && (playerMove != "paper")){
-        response = `You lost! You cannot use ${playerMove}`;
-    }
-    else if (playerMove == compMove){
-        response = `You tie... Both players chose ${compMove}. Rematch!`;
-        win = -1;
+    if (playerMove == compMove){
+        response = `Score [ You(${wins}) : Comp(${losses}) ]\nYou tie... Both players chose ${compMove}. Rematch! `;
     }
     else if ((playerMove == "rock" && compMove == "scissors")
      || (playerMove == "scissors" && compMove == "paper")
      || (playerMove == "paper" && compMove == "rock")){
-        response = `You win the round! ${playerMove} beats ${compMove}.`;
-        win = 1;
+        wins++;
+        response = `Score [ You(${wins}) : Comp(${losses}) ]\nYou win the round! ${playerMove} beats ${compMove}.`;  
      }
      else {
-        response = `You lose the round! ${compMove} beats ${playerMove}.`;
+        losses++;
+        response = `Score [ You(${wins}) : Comp(${losses}) ]\nYou lose the round! ${compMove} beats ${playerMove}.`;
      }
-     outputField.innerText = response;
-     return win;
-}
 
-function playGame(){
-    let wins = 0;
-    let losses = 0;
-    let res = 0;
-    let initGameCount = 5;
-    let gameCount = initGameCount;
-    let response = "";
-    
-    while(true){
-        res = playRound();
-        if (res == -1){
-            gameCount +=1;
-        }
-        else if(res == 1){ 
-            if(++wins > Math.floor(initGameCount / 2)){
-                response = `You won the game! You won ${wins} of ${initGameCount} rounds.`;
-                break;
-            }
-        }
-        else{
-            if(++losses > Math.floor(initGameCount / 2)){
-                response = `You lost the game! You lost ${losses} of ${initGameCount} rounds.`;
-                break;
-            }
-        }
-    }
-    console.log(response);
+     outputField.innerText = response;
+
+     if (wins === 5){
+        outputField.innerText += `\n\nYou won the game! You won ${wins} rounds.`;
+        wins = 0;
+        losses = 0;
+     }
+     else if (losses === 5){
+        outputField.innerText += `\n\nYou lost the game! You lost ${losses} rounds.`;
+        wins = 0;
+        losses = 0;
+     }
 }
